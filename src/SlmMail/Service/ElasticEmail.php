123799a94746cb76a5279959bf1491bb130163c8
Jurian Sluiman <jurian@soflomo.com>
2011-11-25 17:50:59 +0100

    Create ElasticMail service class

---
+++ src/SlmMail/Service/ElasticEmail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SlmMail\Service;
+
+use Zend\Mail\Message,
+    Zend\Http\Client,
+    Zend\Http\Request,
+    Zend\Http\Response;
+
+class ElasticEmail
+{
+    const API_URI = 'https://api.elasticemail.com/';
+
+    protected $apiKey;
+    protected $username;
+    protected $client;
+    
+    public function setApiKey ($api_key)
+    {
+        $this->apiKey = $api_key;
+    }
+    
+    public function setUsername ($username)
+    {
+        $this->username = $username;
+    }
+    
+    public function send (Message $message)
+    {
+        // @todo
+    }
+    
+    public function uploadAttachment () {}
+    public function getStatus () {}
+    public function getLog () {}
+    public function getAccountDetails () {}
+    public function getBounced () {}
+    public function getUnsubscribed () {}
+    
+    protected function getHttpClient ($path)
+    {
+        if (null === $this->client) {
+            $this->client = new Client();
+            $this->client->setUri(self::API_URI)
+                         ->setMethod(Request::METHOD_GET);
+        }
+        
+        $this->client->getUri()->setPath($path);
+        return $this->client;
+    }
+}
