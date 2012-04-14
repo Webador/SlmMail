23621c95d0d6ed0ad90d5250df448f0de272920d
Jurian Sluiman <jurian@soflomo.com>
2012-04-14 12:41:15 +0200

    Add tests for Zend\Mail\Transport instances
    Mock service classes and test if mail is pushed to service

---
+++ tests/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * SlmMail
+ * 
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org/>
+ * 
+ * @category   SlmMail
+ * @copyright  Copyright (c) 2012 Jurian Sluiman <jurian@juriansluiman.nl>
+ * @license    http://unlicense.org Unlicense
+ */
+
+// Set error reporting pretty high
+error_reporting(E_ALL | E_STRICT);
+
+// Get base, application and tests path
+define('BASE_PATH',  dirname(__DIR__));
+
+// Load autoloader
+require_once BASE_PATH . '/autoload_register.php';
