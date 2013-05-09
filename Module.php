1c7548b911b931c907fd3935480b6f099a1b019e
Jurian Sluiman <jurian@soflomo.com>
2013-05-09 11:18:06 +0200

    Remove spaces

--- Module.php
+++ Module.php
@@ -55,7 +55,7 @@ class Module implements
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__           => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
             ),
         );
