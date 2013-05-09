b304d2b18e180e80022720642bd095f662a2a7c5
Jurian Sluiman <jurian@soflomo.com>
2013-05-09 13:53:11 +0200

    Remove import of tag trait

--- src/SlmMail/Mail/Message/Provider/Mailgun.php
+++ src/SlmMail/Mail/Message/Provider/Mailgun.php
@@ -3,7 +3,6 @@
 namespace SlmMail\Mail\Message\Provider;
 
 use SlmMail\Mail\Message\ProvidesOptions;
-use SlmMail\Mail\Message\ProvidesTags;
 use Zend\Mail\Message;
 
 class Mailgun extends Message
